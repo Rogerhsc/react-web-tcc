41f3a2bc2402a72cb2a5c8c9b1979a60a1eced82
roger.conceicao <roger.conceicao@lecom.local>
2019-10-02 17:50:46 -0300

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="content">
+        <div className="rowContent">
+          <div className="itemContent">
+            teste
+          </div>
+          <div className="itemContent">
+            teste
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
