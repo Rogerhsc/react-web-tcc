1ad34e274c76e46c3fa5f8acdd716cc96ffe262e
Roger <rogerhenrique.souza@hotmail.com>
2019-10-06 21:21:26 -0300

    ajustes

--- src/components/returnHeader/index.js
+++ src/components/returnHeader/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Link,  } from "react-router-dom";
 import { ArrowBack } from "@material-ui/icons/";
-import { useLastLocation } from 'react-router-last-location';
 import "./style.css";
 
 export default class ReturnHeader extends Component {
