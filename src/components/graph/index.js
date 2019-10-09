0611fc261543b6b2223176b812f2d9dcc6db53d8
Roger <rogerhenrique.souza@hotmail.com>
2019-10-08 23:48:22 -0300

    Ajuestes grafico ao analitics

--- src/components/graph/index.js
+++ src/components/graph/index.js
@@ -8,37 +8,41 @@ import {
     CartesianGrid,
     Tooltip,
     Legend,
+    ResponsiveContainer,
 } from 'recharts';
 
 
 const data = [
     { name: 'Semana 1', Recebimentos: 800 },
-    { name: 'Semana 2', Recebimentos: 1398},
+    { name: 'Semana 2', Recebimentos: 1398 },
     { name: 'Semana 3', Recebimentos: 550 },
-    { name: 'Semana 4', Recebimentos: 2500},
+    { name: 'Semana 4', Recebimentos: 2500 },
 ];
 export default class Graph extends Component {
     render() {
         return (
             <div className="graphContainer">
-                <LineChart
-                    width={385}
-                    height={275}
-                    data={data}
-                    margin={{top: 45, right: 2}}
-                >
-                    <Line
-                        type='monotone'
-                        dataKey='Recebimentos'
-                        stroke='#ff6600'
-                        activeDot={{ r: 8 }}
-                    />
-                    <CartesianGrid strokeDasharray='3 3' />
-                    <Tooltip />
-                    <YAxis />
-                    <XAxis dataKey='name' />
-                    <Legend />
-                </LineChart>
+                <ResponsiveContainer width="94%" height="100%">
+                    <LineChart
+                        width={385}
+                        height={275}
+                        data={data}
+                        margin={{ top: 45, right: 5 }}
+                    >
+                        <Line
+                            type='monotone'
+                            dataKey='Recebimentos'
+                            stroke='#ff6600'
+                            activeDot={{ r: 8 }}
+                        />
+                        <CartesianGrid strokeDasharray='3 3' />
+                        <Tooltip />
+                        <YAxis />
+                        <XAxis dataKey='name' />
+                        <Legend />
+                    </LineChart>
+                </ResponsiveContainer>
+
             </div>
         )
     }
